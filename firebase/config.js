5de69ab48a8f94d8bcb6fdf8ca0e9b4d40641dad
MaizNadeem <maiznadeem616@gmail.com>
2023-05-27 16:29:30 +0500

    Sanity removed.

---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVv_Mt6KB-YdfuZFeZYffix8TDGTvVSK8",
+  authDomain: "deliveroo-2-c6550.firebaseapp.com",
+  projectId: "deliveroo-2-c6550",
+  storageBucket: "deliveroo-2-c6550.appspot.com",
+  messagingSenderId: "40768926344",
+  appId: "1:40768926344:web:f51d7185f9e4919cb2253d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
